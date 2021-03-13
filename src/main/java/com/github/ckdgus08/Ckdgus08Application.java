a6835f0129efe6f5ca405b1809f3fb0da815d6c6
ckdgus08 <ckdgus0808@naver.com>
2021-03-13 22:34:01 +0900

    git 라이브러리 추가 + log4j2.yml 파일 셋팅

---
+++ src/main/java/com/github/ckdgus08/Ckdgus08Application.java
@@ -0,0 +1,13 @@
+package com.github.ckdgus08;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ckdgus08Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ckdgus08Application.class, args);
+	}
+
+}
