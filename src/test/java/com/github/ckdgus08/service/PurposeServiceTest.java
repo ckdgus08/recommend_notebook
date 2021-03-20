66c6a498f93833ed856f30c4f2f5134f3202b797
ckdgus08 <ckdgus0808@naver.com>
2021-03-21 04:18:42 +0900

    Test : 프로그램에 최소,권장사양(RAM) 추가

--- src/test/java/com/github/ckdgus08/service/PurposeServiceTest.java
+++ src/test/java/com/github/ckdgus08/service/PurposeServiceTest.java
@@ -7,7 +7,6 @@ import com.github.ckdgus08.domain.enum_.*;
 import com.github.ckdgus08.repository.CpuRepository;
 import com.github.ckdgus08.repository.GpuRepository;
 import com.github.ckdgus08.repository.PurposeRepository;
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -16,6 +15,7 @@ import org.springframework.transaction.annotation.Transactional;
 
 import javax.persistence.EntityManager;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatIllegalStateException;
 
 @SpringBootTest
@@ -52,15 +52,23 @@ public class PurposeServiceTest {
 
         em.persist(cpu);
 
-        Gpu gpu = Gpu.builder()
+        Gpu gpu1 = Gpu.builder()
                 .company(GPUType.NVIDIA)
                 .display(4)
                 .model("GTX 1050")
                 .score(5258)
                 .vram(4)
                 .build();
+        Gpu gpu2 = Gpu.builder()
+                .company(GPUType.AMD)
+                .display(4)
+                .model("Radeon RX 580")
+                .score(8803)
+                .vram(8)
+                .build();
 
-        em.persist(gpu);
+        em.persist(gpu1);
+        em.persist(gpu2);
     }
 
     @Test
@@ -77,7 +85,7 @@ public class PurposeServiceTest {
                 SpecLevel.최소사양
         );
         //then
-        Assertions.assertThat(cpu.getModel()).isEqualTo(registered_cpu.getModel());
+        assertThat(cpu.getModel()).isEqualTo(registered_cpu.getModel());
 
         assertThatIllegalStateException().isThrownBy(
                 () -> purposeService.add_require_cpu(
@@ -92,23 +100,62 @@ public class PurposeServiceTest {
     void 프로그램_GPU_최소최저사양_등록() {
         //given
         PurposeType purposeType = PurposeType._테스트;
-        Gpu gpu = gpuRepository.findByModelAndVram("GTX 1050", 4).get(0);
+        Gpu gpu1 = gpuRepository.findByModelAndVram("GTX 1050", 4).get(0);
+        Gpu gpu2 = gpuRepository.findByModelAndVram("Radeon RX 580", 8).get(0);
 
         //when
-        Gpu registered_gpu = purposeService.add_require_gpu(
+        Gpu registered_gpu1 = purposeService.add_require_gpu(
                 purposeType,
-                gpu,
+                gpu1,
+                OS.window,
+                SpecLevel.최소사양
+        );
+        Gpu registered_gpu2 = purposeService.add_require_gpu(
+                purposeType,
+                gpu2,
                 OS.window,
                 SpecLevel.최소사양
         );
         //then
-        Assertions.assertThat(gpu.getModel()).isEqualTo(registered_gpu.getModel());
-        Assertions.assertThat(gpu.getVram()).isEqualTo(registered_gpu.getVram());
+        assertThat(gpu1.getModel()).isEqualTo(registered_gpu1.getModel());
+        assertThat(gpu1.getVram()).isEqualTo(registered_gpu1.getVram());
+        assertThat(gpu2.getModel()).isEqualTo(registered_gpu2.getModel());
+        assertThat(gpu2.getVram()).isEqualTo(registered_gpu2.getVram());
+
+        assertThat(2).isEqualTo(
+                purposeRepository.findByPurposeType(purposeType)
+                        .get().getPurposeGpus().size()
+        );
 
         assertThatIllegalStateException().isThrownBy(
                 () -> purposeService.add_require_gpu(
                         purposeType,
-                        gpu,
+                        gpu1,
+                        OS.window,
+                        SpecLevel.최소사양)
+        );
+    }
+
+    @Test
+    void 프로그램_RAM_최소최저사양_등록() {
+        //given
+        PurposeType purposeType = PurposeType._테스트;
+        Integer ram = 4;
+
+        //when
+        Integer registered_ram = purposeService.add_require_ram(
+                purposeType,
+                ram,
+                OS.window,
+                SpecLevel.최소사양
+        );
+        //then
+        assertThat(ram).isEqualTo(registered_ram);
+
+        assertThatIllegalStateException().isThrownBy(
+                () -> purposeService.add_require_ram(
+                        purposeType,
+                        ram,
                         OS.window,
                         SpecLevel.최소사양)
         );
