2a6d61ad54e0ff1ffaaa98a199c20714fcc6c456
ckdgus08 <ckdgus0808@naver.com>
2021-05-23 21:41:36 +0900

    feat: 스프링 시큐리티 config, user entity, repository 생성

---
+++ src/main/java/com/github/ckdgus08/domain/User.java
@@ -0,0 +1,24 @@
+package com.github.ckdgus08.domain;
+
+import lombok.*;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+@Getter
+@Builder
+@ToString(of = {"id", "userId", "role"})
+public class User extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String userId;
+    private String role;
+}
---
+++ src/main/java/com/github/ckdgus08/repository/UserRepository.java
@@ -0,0 +1,8 @@
+package com.github.ckdgus08.repository;
+
+import com.github.ckdgus08.domain.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+}
